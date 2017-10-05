4c6bd2a828f5cb7ddb01d83096cbfd8f62b2c3ad
Katherine Thompson <katherine.thompson@socialtables.com>
2017-10-05 15:05:40 -0400

    Start writing extension to prettify code elements.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { format } from "prettier";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { highlightAuto } from "lowlight";
+import { github } from "react-syntax-highlighter/dist/styles";
+
+const codeTags = [...document.getElementsByTagName('code')];
+const lowlight = codeTags.forEach(code => {
+  const { language, secondBest } = highlightAuto(code.innerText);
+  if (language === 'javascript' || secondBest.language === 'javascript') {
+    const formatted = format(code.innerText);
+    const highlighted = renderToString(<SyntaxHighlighter style={github} language="javascript">{formatted}</SyntaxHighlighter>);
+    code.innerHTML = highlighted;
+  }
+});
