8cf23afd2d0e2cd94b80d73d6a83943dd7a16975
conorhastings <conor@socialtables.com>
2017-10-05 15:41:05 -0400

    update language check

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { github } from "react-syntax-highlighter/dist/styles";
 const codeTags = [...document.getElementsByTagName('code')];
 const lowlight = codeTags.forEach(code => {
   const { language, secondBest } = highlightAuto(code.innerText);
-  if ((language && language === 'javascript') || (secondBest && secondBest.language === 'javascript')) {
+  const isLanguage = language && language === 'javascript';
+  const isSecondBestLanguage = secondBest && secondBest.language === 'javascript';
+  if (isLanguage || isSecondBestLanguage || code.className.includes("language-js") || code.className.includes("lang-js")) {
     const formatted = format(code.innerText);
     const highlighted = renderToString(<SyntaxHighlighter style={github} language="javascript">{formatted}</SyntaxHighlighter>);
     code.innerHTML = highlighted;
