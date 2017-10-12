57260ff81d3e2fb0a396e86a8212865fef28f41a
conorhastings <conor@socialtables.com>
2017-10-12 13:39:37 -0400

    move manifest and icon to dist

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { highlightAuto } from "lowlight";
 import { github } from "react-syntax-highlighter/dist/styles";
 
+
+
 const codeTags = [...document.getElementsByTagName('code')];
 const lowlight = codeTags.forEach(code => {
   const { language, secondBest } = highlightAuto(code.innerText);
   const isLanguage = language && language === 'javascript';
   const isSecondBestLanguage = secondBest && secondBest.language === 'javascript';
-  if (isLanguage || isSecondBestLanguage || code.className.includes("language-js") || code.className.includes("lang-js")) {
+  if (isLanguage || 
+    isSecondBestLanguage || 
+    code.className.includes("language-js") || 
+    code.className.includes("lang-js") ||
+    code.className.includes("gatsby-code-jsx")
+  ) {
     const formatted = format(code.innerText);
     const highlighted = renderToString(<SyntaxHighlighter style={github} language="javascript">{formatted}</SyntaxHighlighter>);
     code.innerHTML = highlighted;
